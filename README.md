8f1bb1daa61594e4ee5eaee2e181dd3148c38599
DESKTOP-AAP8USC\Rahul <saxenarahul36@gmail.com>
2017-10-07 00:49:55 +0530

    New App

---
+++ README.md
@@ -0,0 +1,10 @@
+# App Component  component for Angular2. 
+*** 
+## Author 
+ Here are the creators of the component: Rahul Saxean 
+## Description 
+ Angular 2 Application
+## Properties 
+- modal
+- 
+
---
+++ protractor.conf.js
@@ -0,0 +1,33 @@
+exports.config = {
+    baseUrl: 'http://localhost:8080/',
+
+    specs: [
+        'src/**/*.e2e-spec.js'
+    ],
+    exclude: [],
+
+    framework: 'jasmine2',
+
+    allScriptsTimeout: 110000,
+
+    jasmineNodeOpts: {
+        showTiming: true,
+        showColors: true,
+        isVerbose: false,
+        includeStackTrace: false,
+        defaultTimeoutInterval: 400000
+    },
+    directConnect: true,
+
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+        jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: true }));
+
+        browser.ignoreSynchronization = true;
+    },
+    useAllAngular2AppRoots: true
+};
